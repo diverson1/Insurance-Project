f4184eafda6702f57e856553b3cfcb9efb9b64d5
diverson1 <diverson@alumni.iastate.edu>
2022-06-01 14:46:19 -0500

    Diving deeper into the data

--- Utilizing_Dataframes_Pandas.py
+++ Utilizing_Dataframes_Pandas.py
@@ -4,12 +4,18 @@ import pandas as pd
 
 
 df= pd.read_csv('https://raw.githubusercontent.com/diverson1/Medical-Insurance-Project/master/insurance.csv')
-print(df)
-df2 = df.groupby('sex').charges.mean()
+print(df.head())
+df2 = df.groupby('sex').charges.mean().reset_index()
+df3 = df.groupby(['sex','bmi','smoker']).charges.mean().reset_index()
+df4 = df.groupby(['sex','bmi', 'age']).charges.mean().reset_index()
+print(df3)
+print(df4)
+
+# bmi and age seems to have very little affect on the cost of insurance incomparision to being a smoker
+# lets compare the min and max bmi levels and their respective charge in comparision to 18yr who does and does not smoke
 
 
 
-print(df2)
 
 
 
