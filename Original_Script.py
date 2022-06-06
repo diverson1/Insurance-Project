da903bdb93b04ba02561e94233dd5a50fb23d69f
diverson1 <diverson@alumni.iastate.edu>
2022-06-05 21:31:24 -0500

    fstrings changes

--- Original_Script.py
+++ Original_Script.py
@@ -37,22 +37,22 @@ average_male_cost = male_cost / male_count
     
 #Converting the averages into more legible output
 
-print('Average cost for individuals under 40 years of age is $' + str(round(average_under_40_cost, 2)))
-print('Average cost for individuals equal to or over 40 years of age is $' + str(round(average_equal_or_over_40_cost, 2)))
-print('Average cost for individuals of male gender is $' + str(round(average_male_cost, 2)))
-print('Average cost for individuals of female gender is $' + str(round(average_female_cost, 2)))
+print(f'Average cost for individuals under 40 years of age is ${average_under_40_cost:,.2f}')
+print(f'Average cost for individuals equal to or over 40 years of age is ${average_equal_or_over_40_cost:,.2f}')
+print(f'Average cost for individuals of male gender is ${average_male_cost:,.2f}')
+print(f'Average cost for individuals of female gender is ${average_female_cost:,.2f}')
 
 
 
 # Calculating sample size make up and which group pays more for insurance
 
 #determining the portion of the sample size men and women both make up, as well as caluculating the percent differnce in the amount they pay
-number_of_females = print('Females account for ' + str(round(female_count/sample_size, 2)*100) + '% of the sample size')
-number_of_males = print('Males account for '+ str(round(male_count/sample_size, 2)*100) + '% of the sample size')
+number_of_females = print(f'Females account for {female_count/sample_size:.2%} of the sample size')
+number_of_males = print(f'Males account for {male_count/sample_size:.2%} of the sample size')
 if average_male_cost > average_female_cost:
-    print('The average insurance cost for men is ' + str(round(((average_male_cost - average_female_cost)/average_female_cost), 2)*100) + '% higher than for women')
+    print(f'The average insurance cost for men is {(average_male_cost - average_female_cost)/average_female_cost:.2%} higher than for women')
 elif average_female_cost > average_male_cost:
-    print('The average insurance cost for women is ' + str(round(((average_female_cost - average_male_cost)/average_male_cost), 2)*100) + '% higher than for men')
+    print(f'The average insurance cost for women is {(average_female_cost - average_male_cost)/average_male_cost:.2%} higher than for men')
 else:
     print('Men and women pay the same amount on average for insurance')
 
